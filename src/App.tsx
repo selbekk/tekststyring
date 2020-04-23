1fd23670379ec88b6130d0542d9fe4d6fefaf2e7
Kristofer Giltvedt Selbekk <kristofer.selbekk@bekk.no>
2020-04-24 00:24:05 +0200

    Flytt browser router ut noen hakk

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,25 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import { AppPage } from "./pages/AppPage";
 import { TextPage } from "./pages/TextPage";
 import { AppListPage } from "./pages/AppListPage";
-import { LandingPage } from "./pages/LandingPage";
 import { CSSReset } from "@chakra-ui/core";
 
 function App() {
   return (
     <div>
       <CSSReset />
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/">
-            <LandingPage />
-          </Route>
-          <Route exact path="/apper">
-            <AppListPage />
-          </Route>
-          <Route exact path="/apper/:appId">
-            <AppPage />
-          </Route>
-          <Route exact path="/apper/:appId/:textKey">
-            <TextPage />
-          </Route>
-        </Switch>
-      </BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <AppListPage />
+        </Route>
+        <Route exact path="/:appId">
+          <AppPage />
+        </Route>
+        <Route exact path="/:appId/:textKey">
+          <TextPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,19 @@ import App from "./App";
 import { theme } from "./theme";
 import { ThemeProvider } from "@chakra-ui/core";
 import { LanguageProvider } from "./components/LanguageContext";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <LanguageProvider supportedLanguages={["nb", "en"]} defaultLanguage="nb">
-        <App />
-      </LanguageProvider>
+      <BrowserRouter>
+        <LanguageProvider
+          supportedLanguages={["nb", "en"]}
+          defaultLanguage="nb"
+        >
+          <App />
+        </LanguageProvider>
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
