110bbf89cba01e61d8f37d5f4d4484a17911b6ad
Kristofer Giltvedt Selbekk <kristofer.selbekk@bekk.no>
2020-04-24 00:25:21 +0200

    Lag et fake API vi kan kalle, og gi det en haug med mock-data

--- src/api.ts
+++ src/api.ts
@@ -1,14 +1,17 @@
+import { mockData } from "./mock-data";
+
 // Dette er et fake API enn så lenge, men det kan jo være sånn vi strukturerer det?
-type TextObjectType = {
+type DbTextObjectType = {
   id: string;
   appId: string;
   language: string;
   textKey: string;
   environment: string;
   value: string;
+  description?: string;
 };
 
-type AppObjectType = {
+type DbAppObjectType = {
   id: string;
   name: string;
   description: string;
@@ -16,112 +19,16 @@ type AppObjectType = {
   languages: string[];
 };
 
-type DbType = { apps: AppObjectType[]; texts: TextObjectType[] };
+export type DbType = { apps: DbAppObjectType[]; texts: DbTextObjectType[] };
 
 export type AppTextsType = {
   [key: string]: string;
 };
 
-const fakeDb = {
-  apps: [
-    {
-      id: "datautlevering",
-      name: "Datautlevering",
-      description: "En app som utleverer data",
-      environments: ["development", "qa", "production"],
-      languages: ["nb", "nn", "en"],
-    },
-    {
-      id: "tekststyring",
-      name: "Tekststyring",
-      description: "Det er denne appen",
-      environments: ["production"],
-      languages: ["nb", "en"],
-    },
-  ],
-  texts: [
-    {
-      id: "1bca",
-      appId: "datautlevering",
-      language: "nb",
-      textKey: "finnEiersKjøretøy.søk.knapp",
-      environment: "development",
-      value: "Søk",
-    },
-    {
-      id: "1bca",
-      appId: "datautlevering",
-      language: "nb",
-      textKey: "finnEiersKjøretøy.søk.tittel",
-      environment: "development",
-      value: "Finn eiers kjøretøy",
-    },
-    {
-      id: "aaaa",
-      appId: "tekststyring",
-      language: "nb",
-      textKey: "landingPage.heading",
-      environment: "production",
-      value: `Velkommen til tekststyringsappen`,
-    },
-    {
-      id: "aaab",
-      appId: "tekststyring",
-      language: "en",
-      textKey: "landingPage.heading",
-      environment: "production",
-      value: `Welcome to the text admin app`,
-    },
-    {
-      id: "aaac",
-      appId: "tekststyring",
-      language: "nb",
-      textKey: "landingPage.description",
-      environment: "production",
-      value: `Denne appen bruker selvfølgelig seg selv for teksthåndtering`,
-    },
-    {
-      id: "aaad",
-      appId: "tekststyring",
-      language: "en",
-      textKey: "landingPage.description",
-      environment: "production",
-      value: `This app uses itself for texts, of course`,
-    },
-    {
-      id: "aaae",
-      appId: "tekststyring",
-      language: "nb",
-      textKey: "landingPage.chooseLanguage.button",
-      environment: "production",
-      value: `Bytt språk`,
-    },
-    {
-      id: "aaaf",
-      appId: "tekststyring",
-      language: "en",
-      textKey: "landingPage.chooseLanguage.button",
-      environment: "production",
-      value: `Change language`,
-    },
-    {
-      id: "1bcd",
-      appId: "tekststyring",
-      language: "en",
-      textKey: "appPage.description",
-      environment: "production",
-      value: `Her vil du finne alle tekstene til {appName}. De er sortert etter "nøkkel", men du kan også søke etter teksten du leter etter.`,
-    },
-    {
-      id: "1bcf",
-      appId: "tekststyring",
-      language: "nb",
-      textKey: "appPage.description",
-      environment: "production",
-      value: `Here you'll find all the texts for {appName}. They are sorted after their "key", but you can also search for the text you're looking for`,
-    },
-  ],
-} as DbType;
+if (!localStorage.getItem("db")) {
+  localStorage.setItem("db", JSON.stringify(mockData));
+}
+const fakeDb = JSON.parse(localStorage.getItem("db")!) as DbType;
 
 const fakeGet = async <T>(returnValue: T): Promise<T> =>
   new Promise((resolve) => setTimeout(() => resolve(returnValue), 500));
@@ -129,13 +36,29 @@ const fakeGet = async <T>(returnValue: T): Promise<T> =>
 /** Henter alle apper */
 export const getApps = async () => fakeGet(fakeDb.apps);
 
+/** Henter en app basert på app ID */
+export const getApp = async (appId: string) =>
+  fakeGet(fakeDb.apps.find((app) => app.id === appId));
+
 /** Henter alle tekstobjekter for en app id */
-export const getTextObjectsForAppId = async (appId: string) =>
-  fakeGet(fakeDb.texts.filter((text) => text.appId === appId));
+export const getTextKeysForAppId = async (appId: string) =>
+  fakeGet(
+    Array.from(
+      new Set(
+        fakeDb.texts
+          .filter((text) => text.appId === appId)
+          .map((text) => text.textKey)
+      )
+    ).sort()
+  );
 
 /** Henter tekstobjektet for en gitt nøkkel */
-export const getTextObject = async (textKey: string) =>
-  fakeGet(fakeDb.texts.find((text) => text.textKey === textKey));
+export const getTextObject = async (appId: string, textKey: string) =>
+  fakeGet(
+    fakeDb.texts.filter(
+      (text) => text.appId === appId && text.textKey === textKey
+    )
+  );
 
 /** Henter alle tekster for en gitt app, miljø og språk */
 export const getTexts = async (
@@ -155,3 +78,40 @@ export const getTexts = async (
       // textKey som key og value som verdi.
       .reduce((acc, next) => ({ ...acc, [next.textKey]: next.value }), {})
   );
+
+/** Legger til eller oppdaterer et tekstobjekt i "databasen" */
+export const createOrUpdateTexts = (
+  appId: string,
+  textKey: string,
+  texts: { [key: string]: string }
+) => {
+  Object.entries(texts).forEach(([languageAndEnvironment, value]) => {
+    const [language, environment] = languageAndEnvironment.split("-");
+    const existingIndex = fakeDb.texts.findIndex(
+      (entry) =>
+        entry.appId === appId &&
+        entry.textKey === textKey &&
+        entry.language === language &&
+        entry.environment === environment
+    );
+    if (existingIndex !== -1) {
+      fakeDb.texts[existingIndex].value = value;
+    } else {
+      fakeDb.texts.push({
+        id: String(Math.random()).substring(2), // fattigmanns UUID
+        appId,
+        environment,
+        language,
+        textKey,
+        value,
+      });
+    }
+  });
+  localStorage.setItem("db", JSON.stringify(fakeDb));
+};
+
+/** Sletter en tekst og alle dens oversettelser */
+export const deleteText = (textKey: string) => {
+  fakeDb.texts = fakeDb.texts.filter((text) => text.textKey !== textKey);
+  localStorage.setItem("db", JSON.stringify(fakeDb));
+};
